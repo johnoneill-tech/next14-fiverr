29843ac1714273d2b4bdef5a7254cb73c56a2091
John O'Neill <john_oneill@hotmail.ca>
2024-08-21 01:15:22 +0000

    auth and database

---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+      {
+        domain: "https://immense-moose-55.clerk.accounts.dev/npx shadcn-ui@latest add button accordion alert-dialog aspect-ratio avatar card carousel checkbox dialog dropdown-menu form-input label navigation-menu pagination popover progress scroll-area select separatorsheet skeleton sonner table tabs textarea toast toaster tooltip",
+        applicationID: "convex",
+      },
+    ]
+  };
